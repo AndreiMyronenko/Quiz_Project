6b0df9f547889cbccf5de34c98cf000e61510594
Andrew <lordpotter95@gmail.com>
2022-01-29 14:20:43 +0100

    Added filtere dataTable

--- quiz/static/js/results.js
+++ quiz/static/js/results.js
@@ -1,3 +1,67 @@
 $(document).ready(function() {
-    $('#results').DataTable();
+    $('#results thead tr')
+        .clone(true)
+        .addClass('filters')
+        .appendTo('#results thead');
+
+    var table = $('#results').DataTable({
+        "order": [
+            [2, "desc"]
+        ],
+        orderCellsTop: true,
+        fixedHeader: true,
+        initComplete: function() {
+            var api = this.api();
+
+            // For each column
+            api
+                .columns()
+                .eq(0)
+                .each(function(colIdx) {
+                    // Set the header cell to contain the input element
+                    var cell = $('.filters th').eq(
+                        $(api.column(colIdx).header()).index()
+                    );
+                    var title = $(cell).text();
+                    $(cell).html('<input type="text" placeholder="' + title + '" />');
+
+                    // On every keypress in this input
+                    $(
+                            'input',
+                            $('.filters th').eq($(api.column(colIdx).header()).index())
+                        )
+                        .off('keyup change')
+                        .on('keyup change', function(e) {
+                            e.stopPropagation();
+
+                            // Get the search value
+                            $(this).attr('title', $(this).val());
+                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
+
+                            var cursorPosition = this.selectionStart;
+                            // Search the column for that value
+                            api
+                                .column(colIdx)
+                                .search(
+                                    this.value != '' ?
+                                    regexr.replace('{search}', '(((' + this.value + ')))') :
+                                    '',
+                                    this.value != '',
+                                    this.value == ''
+                                )
+                                .draw();
+
+                            $(this)
+                                .focus()[0]
+                                .setSelectionRange(cursorPosition, cursorPosition);
+                        });
+                });
+        },
+    });
 });
+//    $('#results').DataTable({
+//        "order": [
+//            [2, "desc"]
+//        ]
+//    })
+//});
